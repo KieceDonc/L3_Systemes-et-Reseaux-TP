646c66bfd601c942ee5b6a16c4a2babff72c49e4
KieceDonc <valentinverst.developer@gmail.com>
2021-11-12 16:41:01 +0100

    adding features

--- Projet/GestionJeu.sh
+++ Projet/GestionJeu.sh
@@ -1,14 +1,57 @@
 #!/bin/bash
 
-echo -n "Entrer le nombre de joueur : "
-read NBPLAYERS
+declare -i LAST_CARD_INDEX=0 # On déclare un integer. Il décrit l'index de la dernière envoyé à un joueur dans le tableau CARDS 
 
-for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
-  xterm -e "./JoueurHumain.sh $x" &
-  mkfifo $x.pipe
-done
+function InitAndRandomlySortCards(){
+  # Initialisation du tableau 
+  CARDS=()
 
-for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
-  echo "test pipe PLAYER_ID = "$x > $x.pipe 
-done
+  # On ajoute les cartes de 1 à 100 au tableau
+  for x in {1..100};do
+    CARDS+=($x)
+  done
 
+  # On échange une carte à l'index entre 0 à 99 avec une autre carte à l'index entre 0 et 99
+  for x in {1..100};do
+    RANDOM0=$((RANDOM%99))
+    RANDOM1=$((RANDOM%99))
+    TMP=${CARDS[$RANDOM0]}
+    CARDS[$RANDOM0]=${CARDS[$RANDOM1]}
+    CARDS[$RANDOM1]=$TMP
+  done
+
+  #echo ${CARDS[@]}
+}
+
+function InitPlayers(){
+  # On demande le nombre de joueur
+  echo -n "Entrer le nombre de joueur : "
+  read NBPLAYERS
+
+  # On initialise les terminaux + pipes
+  for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
+    xterm -e "./JoueurHumain.sh $x" & # Initialisation des terminaux en donnant en paramètre le n° du joueur
+    mkfifo $x.pipe # Initialisation des pipes qui prennent le nom "n°Joueur.pipe"
+  done
+}
+
+function SendCardsToPlayers(){
+  for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
+    echo ${CARDS[$LAST_CARD_INDEX]} > $x.pipe 
+    LAST_CARD_INDEX+=1
+  done
+}
+
+function ListenPipe(){
+  mkfifo gestionJeu.pipe
+}
+
+function removePipe(){
+  rm *.pipe
+}
+
+InitAndRandomlySortCards
+InitPlayers
+SendCardsToPlayers
+read tmp
+removePipe
--- Projet/JoueurHumain.sh
+++ Projet/JoueurHumain.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
 PLAYER_ID=$1
-echo "PLAYER ID = "$PLAYER_ID
-cat $PLAYER_ID.pipe
-read tmp
+declare -i ROUND=1 # On déclare un integer. Il décrit le numéro du tour
+
+function askCardToPlay(){
+  CANT_PLAY_THIS_CARD=true # On initialise un booléen qui va servir de drapeau pour savoir si l'entrée utilisateur est bonne
+  while $CANT_PLAY_THIS_CARD # Tant que l'entrée utilisateur est mauvaise on répète
+  do
+    echo -n "Entrer la carte que vous souhaiter jouer : "
+    read CARD_TOPLAY # On demande au joueur d'input la carte qu'il souhaite jouer, CARD_TOPLAY = Carte choisit par l'utilisateur et qui doit être jouer
+    for CURRENT_CARD in "${CURRENT_CARDS[@]}";do # On vérifie si elle est présente dans son jeu parmit toutes ses cartes
+      if [ $CARD_TOPLAY -eq $CURRENT_CARD ];then # On vérifie si la carte courante est présente dans son jeu
+        CANT_PLAY_THIS_CARD=false # elle est présente
+      fi
+    done  
+    if [ "$CANT_PLAY_THIS_CARD" = true ]; then # Si l'entrée est mauvaise on lui montre ces cartes 
+      echo "Impossible de jouer cette, vos cartes sont :" ${CURRENT_CARDS[@]}
+    fi
+  done
+}
+
+function waitForCards(){
+  CURRENT_CARDS=() # Contient les cartes du joueur
+  for x in $( eval echo {1..$(($ROUND))} );do
+    INCOMING_CARD=$(cat $PLAYER_ID.pipe)
+    CURRENT_CARDS+=($INCOMING_CARD)
+  done
+  echo "Cartes reçut ! Vos cartes : "${CURRENT_CARDS[@]} 
+}
+
+echo "Vous êtes le joueur n°"$PLAYER_ID
+echo "En attente de vos cartes ..."
+waitForCards
+askCardToPlay
