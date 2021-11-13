21be56a6e17a8a2e9eb10568789d8515da68bc1b
KieceDonc <valentinverst.developer@gmail.com>
2021-11-13 13:17:53 +0100

    GestionJeu and JoueurHumain finish, must be test

--- Projet/GestionJeu.sh
+++ Projet/GestionJeu.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
+ 
+CARDS=() # Tableau qui contient les cartes mélangées
 declare -i LAST_CARD_INDEX=0 # On déclare un integer. Il décrit l'index de la dernière envoyé à un joueur dans le tableau CARDS 
 declare -i ROUND=1 # On déclare un integer. Il décrit le numéro du tour
 CURRENT_ROUND_SORTED_CARDS=() # Liste des cartes tirer et trier pour le tour courant
 declare -i CURRENT_ROUND_INDEX=0 # On déclare un integer. Il décrit l'index de la carte que l'on doit trouver pour le round courant
+NBPLAYERS=0 # Décrit le nombre de joueur
+NBROBOT=0 # Décrit le nombre de robot
+declare -i MAX_ROUND=0 # On déclare un integer. Décrit le nombre maximun de tour
 
 function InitAndRandomlySortCards(){
-  # Initialisation du tableau 
+  
+  # On initialise les cartes
   CARDS=()
 
   # On ajoute les cartes de 1 à 100 au tableau
@@ -38,13 +43,25 @@ function InitPlayers(){
   done
 }
 
+function InitMaxRound(){
+  MAX_ROUND=0
+  NOT_FOUND=true # On initialise un booléen qui va servir de drapeau pour savoir si on a trouver le nombre maximum de tour
+  while $NOT_FOUND 
+  do
+    if [ $(($MAX_ROUND*$NBPLAYERS)) -le $((100)) ];then # On vérifie si le nombre de carte distribués est inférieur ou égale à 100
+      MAX_ROUND+=1 # On peut rajouter un tour
+    else
+      NOT_FOUND=false # On a trouver le nombre max de tour
+    fi
+  done
+}
+
 function SortCurrentRoundCards(){
   UNSORTED_CARDS_LENGTH=$(($ROUND*$NBPLAYERS-1))
   for x in $( eval echo {0..$(($ROUND*$NBPLAYERS-1))} );do
     CURRENT_MINUS=1000
     for y in $( eval echo {0..$UNSORTED_CARDS_LENGTH} );do 
       CURRENT_CARD=${CURRENT_ROUND_UNSORTED_CARDS[y]} # Carte courante de la liste des cartes non trier
-      echo $CURRENT_CARD
       if [ $(($CURRENT_CARD)) -lt $(($CURRENT_MINUS)) ];then # On vérifie si la carte courante est inférieur au minimun courant
         CURRENT_MINUS=$CURRENT_CARD
       fi
@@ -57,31 +74,56 @@ function SortCurrentRoundCards(){
 }
 
 function SendCardsToPlayers(){
-  for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
-    for y in $( eval echo {1..$ROUND} );do
-      CURRENT_CARD=${CARDS[$LAST_CARD_INDEX]}
-      $(echo $CURRENT_CARD > $x.pipe) 
-      CURRENT_ROUND_UNSORTED_CARDS+=($CURRENT_CARD)
-      LAST_CARD_INDEX+=1
+  for x in $( eval echo {0..$(($NBPLAYERS-1))} );do # Pour chaque joueur 
+    for y in $( eval echo {1..$ROUND} );do # Pour le numéro de carte que l'on doit envoyé
+      CURRENT_CARD=${CARDS[$LAST_CARD_INDEX]} # On recupère une carte 
+      $(echo "0;"$CURRENT_CARD > $x.pipe)  # On l'envoit au joueur
+      CURRENT_ROUND_UNSORTED_CARDS+=($CURRENT_CARD) # On indique dans une liste non trier qu'une nouvelle carte est dans le jeu
+      LAST_CARD_INDEX+=1 # On incrémente l'index qui décrit le n° de la carte envoyé à un joueur
+      echo "card $CURRENT_CARD sent to $X"
     done
+    $(echo "5;" > $x.pipe)  # On notifie que toutes les cartes ont été envoyées
+    echo "cards sent to $X"
   done
-  SortCurrentRoundCards
+  SortCurrentRoundCards # On trie du plus petit au plus grand les cartes envoyé aux joueurs 
+}
+
+function updateFoundedCards(){
+  FOUNDED_CARDS="( " # On prépare l'affichage de toutes les cartes trouver
+  for x in $( eval echo {0..$CURRENT_ROUND_INDEX);do # On affiche toutes les cartes trouver
+    FOUNDED_CARDS=FOUNDED_CARDS+"${CURRENT_ROUND_SORTED_CARDS[CURRENT_ROUND_INDEX]} "
+  done
+  FOUNDED_CARDS=FOUNDED_CARDS+")"
 }
 
 function ListenPipe(){
   mkfifo gestionJeu.pipe
-  INCOMING_CARD=$(cat $PLAYER_ID.pipe)
-  WINNING_CARD=${CURRENT_ROUND_SORTED_CARDS[CURRENT_ROUND_INDEX]}
+  INCOMING_CARD=$(cat gestionJeu.pipe) # On récupère la carte reçus
+  WINNING_CARD=${CURRENT_ROUND_SORTED_CARDS[CURRENT_ROUND_INDEX]} # On récupère la carte à trouvée
   if [ $(($WINNING_CARD)) -eq $(($INCOMING_CARD)) ];then
-    #for x in $( eval echo {0..$(($NBPLAYERS-1))} )
-    #$(echo "Bravo, la carte $WINNING_CARD a été trouvés, voici les cartes trouvées : " > $x.pipe)
-    #CURRENT_ROUND_INDEX+=1
-  else
-    for x in $( eval echo {0..$(($NBPLAYERS-1))} );do
-      $(echo "Perdu, la carte $INCOMING_CARD n'était pas la bonne, la bonne était : $WINNING_CARD" > $x.pipe) 
+    updateFoundedCards
+    for x in $( eval echo {0..$(($NBPLAYERS-1))} );do # On envoit un message à tout les joueurs disant que la carte trouvée était la bonne
+      $(echo "1;Bravo, la carte $WINNING_CARD a été trouvés, voici les cartes trouvées : $FOUNDED_CARDS" > $x.pipe)
     done
+    CURRENT_ROUND_INDEX+=1 # Le tour continue, on incrémente l'index de la prochaine carte à trouvée
+    if [ $(($CURRENT_ROUND_INDEX)) -eq $(($ROUND*$NBPLAYERS)) ];then # On vérifie si la dernière carte trouvée correspond à la dernière carte pouvant être jouer ce tour ( on vérifie si le tour est terminé )
+      if [ $(($ROUND*$NBPLAYERS)) -le $((100)) ];then # On vérifie si il reste un tour
+        $(echo "3;Félications, le tour n°$ROUND est terminé, on passe au tour suivant" > $x.pipe)
+        ROUND+=1
+        SendCardsToPlayers
+     else
+        $(echo "4;Félications, le jeu est terminé" > $x.pipe)
+        removePipe
+        exit
+      fi
+    fi
+  else
+    for x in $( eval echo {0..$(($NBPLAYERS-1))} );do # On envoit un message à tout les joueurs disant que la carte trouvée était la mauvaise
+      $(echo "2;Perdu, la carte $INCOMING_CARD n'était pas la bonne, la bonne était : $WINNING_CARD. On recommence !" > $x.pipe) 
+    done
+    CURRENT_ROUND_INDEX=0 # Le tour recommence, on réinitialise l'index de la prochaine carte à trouvée
   fi
-
+  ListenPipe
 }
 
 function removePipe(){
@@ -90,6 +132,6 @@ function removePipe(){
 
 InitAndRandomlySortCards
 InitPlayers
+InitMaxRound
 SendCardsToPlayers
-read tmp
-removePipe
+ListenPipe
--- Projet/JoueurHumain.sh
+++ Projet/JoueurHumain.sh
@@ -2,8 +2,10 @@
 
 PLAYER_ID=$1
 declare -i ROUND=1 # On déclare un integer. Il décrit le numéro du tour
+CURRENT_CARDS=() # Contient les cartes du joueur
+declare -i NB_CARDS # On déclare un integer. Il décrit le nombre de carte en main
 
-function askCardToPlay(){
+function ListenCardToPlay(){
   CANT_PLAY_THIS_CARD=true # On initialise un booléen qui va servir de drapeau pour savoir si l'entrée utilisateur est bonne
   while $CANT_PLAY_THIS_CARD # Tant que l'entrée utilisateur est mauvaise on répète
   do
@@ -14,22 +16,61 @@ function askCardToPlay(){
         CANT_PLAY_THIS_CARD=false # elle est présente
       fi
     done  
-    if [ "$CANT_PLAY_THIS_CARD" = true ]; then # Si l'entrée est mauvaise on lui montre ces cartes 
-      echo "Impossible de jouer cette, vos cartes sont :" ${CURRENT_CARDS[@]}
+    if [ $NB_CARDS -eq $((0)) ];then
+      echo "Vous n'avez aucune carte a joué"
+    else
+      if [ "$CANT_PLAY_THIS_CARD" = true ]; then # Si l'entrée est mauvaise on lui montre ces cartes 
+        echo "Impossible de jouer cette carte, vos cartes sont :" ${CURRENT_CARDS[@]}
+      fi
     fi
   done
+
+  OLD_CURRENT_CARDS=CURRENT_CARDS
+  CURRENT_CARDS=()
+  for x in $( eval echo {0..$(($NB_CARDS))} );do
+    OLD_CURRENT_CARD=$(($OLD_CURRENT_CARDS[x]));
+    if [ $CARD_TOPLAY -ne $OLD_CURRENT_CARD ];then
+      CURRENT_CARDS+=$OLD_CURRENT_CARD
+    fi
+  done
+  NB_CARDS=$(($NB_CARDS-1))
+
+  echo $CARD_TOPLAY > gestionJeu.pipe
 }
 
-function waitForCards(){
-  CURRENT_CARDS=() # Contient les cartes du joueur
-  for x in $( eval echo {1..$ROUND} );do
-    INCOMING_CARD=$(cat $PLAYER_ID.pipe)
-    CURRENT_CARDS+=($INCOMING_CARD)
-  done
-  echo "Cartes reçut ! Vos cartes : "${CURRENT_CARDS[@]} 
+function ListenPipe(){
+  ListenCardToPlay
+  INCOMING_DATA=$(cat $PLAYER_ID.pipe)
+  SPLIT_DATA=(${INCOMING_DATA//;/ }) # https://stackoverflow.com/a/5257398
+  API_CALL=SPLIT_DATA[0]
+  API_MESSAGE=SPLIT_DATA[1]
+
+  if [ $(($API_CALL)) -eq $((0)) ];then # Une carte a été reçu
+      INCOMING_CARD=$(cat $PLAYER_ID.pipe)
+      CURRENT_CARDS+=($INCOMING_CARD)
+      NB_CARDS+=1
+      ListenPipe
+  elif [ $(($API_CALL)) -eq $((5)) ];then # Toutes les cartes ont été reçu
+    echo "Cartes reçut ! Vos cartes : "${CURRENT_CARDS[@]}
+    ListenPipe
+  elif [ $(($API_CALL)) -eq $((1)) ];then # Une carte du tour courant a été trouvé
+    echo $API_MESSAGE
+    ListenPipe
+  elif [ $(($API_CALL)) -eq $((2)) ];then # Une mauvaise carte a été trouvé, le tour recommence
+    echo $API_MESSAGE
+    CURRENT_CARDS=()
+    ListenPipe
+  elif [ $(($API_CALL)) -eq $((3)) ];then # Le tour est terminé, on passe au tour suivant
+    echo $API_MESSAGE
+    CURRENT_CARDS=()
+    ListenPipe
+  elif [ $(($API_CALL)) -eq $((4)) ];then # Le jeu est terminé
+    echo $API_MESSAGE
+    exit
+  fi
+
 }
 
 echo "Vous êtes le joueur n°"$PLAYER_ID
 echo "En attente de vos cartes ..."
-waitForCards
-askCardToPlay
+ListenPipe
